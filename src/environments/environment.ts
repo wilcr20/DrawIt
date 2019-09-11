0e1673d8471a40df8ca28470e5f6f0a78c046a3c
Wilfred <wilcr20@gmail.com>
2019-09-11 12:18:24 -0600

    api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,7 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyBPnoaiVn38Zk7bAkqOzX4LyL37nAFbqpM",
-  authDomain: "draw-it-cr.firebaseapp.com",
-  databaseURL: "https://draw-it-cr.firebaseio.com",
-  projectId: "draw-it-cr",
-  storageBucket: "",
-  messagingSenderId: "907251174561",
-  appId: "1:907251174561:web:10532565d78fc83b"
+ 
 };
 
 /*
